ff46649009a092ec6a5235ddcb0822b498367285
Pablo Fino <pablo.fino@globant.com>
2021-07-13 12:04:56 -0300

    Final Avanzada 2021

--- src/main/java/edu/utn/FinalAvanzada/controller/Controller.java
+++ src/main/java/edu/utn/FinalAvanzada/controller/Controller.java
@@ -20,38 +20,5 @@ import java.util.List;
 @RequestMapping("/")
 public class Controller {
 
-    @Autowired
-    private Connection conn;
 
-    @GetMapping("cities")
-    public ResponseEntity<List<City>> getCities() {
-        try {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM cities");
-            List<City> cities = new ArrayList<>();
-            while (rs.next()) {
-                cities.add(City.builder().code(rs.getString("iata_code")).name(rs.getString("city_name")).build());
-            }
-            return ResponseEntity.ok(cities);
-
-        } catch (SQLException e) {
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    @GetMapping("employees")
-    public ResponseEntity<List<Employee>> getEmployees() {
-        try {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM employees e inner join cities c on e.city_iata_code = c.iata_code");
-            List<Employee> employees = new ArrayList<>();
-            while (rs.next()) {
-                employees.add(Employee.builder().id(rs.getInt("id")).name(rs.getString("name")).lastName("last_name").age(rs.getInt("age")).city(
-                        City.builder().code(rs.getString("iata_code")).name(rs.getString("city_name")).build()).build());
-            }
-            return ResponseEntity.ok(employees);
-        } catch (SQLException e) {
-            return ResponseEntity.badRequest().build();
-        }
-    }
 }
